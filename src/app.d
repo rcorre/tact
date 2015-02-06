86ad73dbfc27a410f94545f85cf2363b1647ab53
Ryan <ryan@rcorre.net>
2015-02-06 18:43:07 -0500

    config loading

--- src/app.d
+++ src/app.d
@@ -2,9 +2,16 @@ import std.stdio;
 import config;
 import interpreter;
 
+version (Windows) {
+  static assert(0, "don't know where windows home dir is!");
+}
+else {
+  private enum configPath = "~/.tactrc";
+}
+
 void main(string[] args) {
   bool foundConfig;
-  auto cfg = Config.load;
+  auto cfg = Config.load(configPath);
   writeln(cfg.storageDir);
   //interpretCommand(args); // strip executable name
 }
--- src/config.d
+++ src/config.d
@@ -2,36 +2,93 @@ module config;
 
 import std.path;
 import std.file;
-import std.exception : enforce;
 import dini;
 
 version(Windows) {
   static assert(0, "don't know where windows home dir is");
 }
 else {
-  private enum {
-    defaultStorageDir = "~/.tact",
-    configPath        = "~/.tactrc"
+  private enum defaultStorageDir = "~/.tact";
+}
+
+/// keywords used to identify transaction and query parameters
+enum defaultKeywords = [
+  "amount"    : "amount", /// the quantity of money in a transaction
+  "source"    : "from",   /// the source (sender) of a transaction
+  "dest"      : "to",     /// the destination (recipient) of a transaction
+  "date"      : "on",     /// to date on which a transaction occured
+  "note"      : "note",   /// a note about the transaction
+  "endDate"   : "before", /// latest date to include in query
+  "startDate" : "after",  /// earliest date to include in query
+];
+
+struct Config {
+  /// directory where transaction records should be stored
+  string storageDir = defaultStorageDir;
+  string[string] keywords;
+
+  this(Ini ini) {
+    storageDir = ini.getKey("storageDir");
+
+    if (ini.hasSection("keywords")) {
+      auto keywordSection = ini.getSection("keywords");
+      foreach(key, val ; defaultKeywords) {
+        keywords[key] = (key in keywordSection.keys) ?
+          keywordSection.getKey(key) :
+          defaultKeywords[key];
+      }
+    }
+  }
+
+  /// load from ini
+  static Config load(string path) {
+    Config cfg;
+    auto expandedPath = path.expandTilde;
+
+    if (expandedPath.exists) {
+      cfg = Config(Ini.Parse(expandedPath));
+    }
+
+    return cfg;
   }
 }
 
-struct Config {
-  /// members with defaults
-  string storageDir = defaultStorageDir;
-
-  /// load from ini
-  this(Ini ini) {
-    storageDir = ini.getKey("storageDir");
-  }
-
-  /// returns config loaded from conigPath
-  /// returns a default-constructed config if the config file is not found
-  static Config load() {
-    Config cfg; // defaul constructed cfg
-    auto path = configPath.expandTilde;
-    if (path.exists) {
-      cfg = Config(Ini.Parse(path));
+unittest {
+  import std.file;
+  import std.path;
+  import std.conv : text;
+  import std.algorithm : joiner;
+  // set up temp dir to load from
+  auto cfgDir  = buildPath(tempDir(), "tact_unittest");
+  auto cfgPath = buildPath(cfgDir, "tactrc");
+  assert(!cfgDir.exists, "unittest failure: " ~ cfgDir ~ " already exists");
+  cfgDir.mkdirRecurse;
+  scope(exit) {
+    if (cfgDir.exists) {
+      cfgDir.rmdirRecurse;
     }
-    return cfg;
   }
+
+  // write out a mock config file
+  auto cfgText = [
+    "storageDir = ~/my_custom_dir/tact",
+
+    "[keywords]",
+    "amount  = price",
+    "date    = date",
+    "note    = description",
+    "invalid = notakeyword",
+  ].joiner("\n");
+
+  cfgPath.write(cfgText.text);
+
+  auto cfg = Config.load(cfgPath);
+  assert(cfg.storageDir == "~/my_custom_dir/tact");
+
+  auto expectedKeywords = defaultKeywords;
+  expectedKeywords["amount"] = "price";
+  expectedKeywords["date"]   = "date";
+  expectedKeywords["note"]   = "description";
+
+  assert(cfg.keywords == expectedKeywords);
 }
